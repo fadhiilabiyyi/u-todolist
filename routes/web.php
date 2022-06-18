bff4850413e0e2d3eabc21122c00a801007d09ad
fadhiilabiyyi <73749422+fadhiilabiyyi@users.noreply.github.com>
2022-06-18 17:46:44 +0700

    Adding route for create function

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(TaskController::class)->group(function() {
+    Route::get('/dashboard', 'index');
+    Route::post('/dashboard', 'store')->name('store');
+    Route::post('/dashboard/edit', 'update')->name('update');
 });
