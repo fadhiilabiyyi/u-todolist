f83c7900ee4c56fcbc5d9b89258ec452f5c1c0d8
fadhiilabiyyi <73749422+fadhiilabiyyi@users.noreply.github.com>
2022-06-19 13:04:23 +0700

    create shortcut to open modal create

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,3 +4,14 @@ const myInput = document.getElementById("task_name_input");
 myModal.addEventListener("shown.bs.modal", () => {
     myInput.focus();
 });
+
+// Shorcut open create modal | Ctrl + /
+document.addEventListener("keydown", (e) => {
+    if (e.key.toLowerCase() === "/" && e.ctrlKey) {
+        let myModal = new bootstrap.Modal(
+            document.getElementById("ModalCreate"),
+            {}
+        );
+        myModal.show();
+    }
+});
