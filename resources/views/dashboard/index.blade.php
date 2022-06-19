814d3a5c8c1c556b5c8b78a1180257ae26e65aea
fadhiilabiyyi <73749422+fadhiilabiyyi@users.noreply.github.com>
2022-06-19 13:01:30 +0700

    create modal for update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -30,27 +30,32 @@
                 </table>
 
                 <!-- Button trigger modal -->
-                <button type="button" class="btn btn-primary mb-5" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <button type="button" class="btn btn-primary mb-5" data-bs-toggle="modal" data-bs-target="#ModalCreate">
                     Tugas Baru
+                </button>
+
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary mb-5" data-bs-toggle="modal" data-bs-target="#ModalUpdate">
+                    Update
                 </button>
             </div>
         </div>
   
         <!-- Modal for Create -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="ModalCreate" tabindex="-1" aria-labelledby="ModalCreateLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <!-- Create new Task -->
                     <form action="{{ route('store') }}" method="post" autocomplete="off">
                         @csrf
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Menambah Tugas Baru</h5>
+                            <h5 class="modal-title" id="ModalCreateLabel">Menambah Tugas Baru</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             <div class="mb-3">
                                 <label for="task_name" class="form-label">Nama Tugas</label>
-                                <input type="text" class="form-control" id="task_name" name="task_name" aria-describedby="emailHelp">
+                                <input type="text" class="form-control" id="task_name_input" name="task_name" aria-describedby="emailHelp" autofocus>
                                 <input type="hidden" name="finished" value="0">
                             </div>
                         </div>
@@ -63,5 +68,33 @@
             </div>
         </div>
         <!-- End of Modal (Create) -->
+
+        <!-- Modal for Update -->
+        <div class="modal fade" id="ModalUpdate" tabindex="-1" aria-labelledby="ModalUpdateLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <!-- Update a Task -->
+                    <form action="" method="post" autocomplete="off">
+                        @csrf
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="ModalCreateLabel">Update Tugas</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="task_name_update" class="form-label">Nama Tugas</label>
+                                <input type="text" class="form-control" id="task_name_update" name="task_name" aria-describedby="emailHelp" disabled>
+                                <input type="hidden" name="finished" value="1">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modalUpdate">Tutup</button>
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- End of Modal (Update) -->
     </div>
 @endsection
